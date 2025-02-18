b3857097f56fb9b6a6df1ff986062c020ac02f89
GallantJoseph <gallant.joseph@outlook.com>
2025-02-18 13:16:01 -0330

    Menu
    Started the menu page.
    Reading the data from the JSON file and generate each element.

---
+++ scripts/menu.js
@@ -0,0 +1,57 @@
+/*
+  Description: Midterm Sprint - Code Brew Café
+  Authors:  Ashton Dennis,
+            Joseph Gallant,
+            Justin Greenslade
+  Dates:  February 17, 2025 - 
+*/
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Read menu from JSON file
+
+  function generateMenu() {
+    let menuPromise = fetch("../data/menu.json");
+
+    menuPromise
+      .then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            createMenuElement(data);
+          });
+        }
+      })
+      .catch((reject) => alert(reject));
+  }
+
+  function createMenuElement(data) {
+    data.forEach((element) => {
+      let menuElement = document.createElement("div");
+      menuElement.className = "menu-item";
+
+      let image = document.createElement("img");
+      image.width = 350;
+      image.height = 350;
+      image.src = element.itemImage;
+      image.alt = element.itemName;
+
+      menuElement.appendChild(image);
+
+      let itemPrice = element.itemPrice;
+
+      let menuElementHTML = `<h3>${element.itemName}</h3>
+                             <p>${element.itemDesc}</p>
+                             <h4>${element.itemPrice}</h4>
+                             <a class="menubutton" href="#" onclick="event.preventDefault}">Add</a>`;
+
+      menuElement.innerHTML += menuElementHTML;
+
+      document.querySelector("#menu-section").appendChild(menuElement);
+    });
+  }
+
+  function addItem(id, evt) {
+    evt.preventDefault();
+  }
+
+  generateMenu();
+});
---
+++ scripts/order.js
@@ -0,0 +1,92 @@
+/*
+  Description: Midterm Sprint - Code Brew Café
+  Authors:  Ashton Dennis,
+            Joseph Gallant,
+            Justin Greenslade
+  Dates:  February 17, 2025 - 
+*/
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#orderBtn").addEventListener("click", () => {
+    submitOrder()
+      .then((resolve) => {
+        displayStatus(resolve, "success");
+      })
+      .catch((reject) => {
+        displayStatus(reject, "fail");
+      });
+  });
+
+  function submitOrder() {
+    return new Promise((resolve, reject) => {
+      try {
+        validateOrderFields();
+
+        resolve("Order Submitted");
+      } catch (error) {
+        reject(error.message);
+      }
+    });
+  }
+
+  function validateOrderFields() {
+    let name = document.querySelector("#userFNameTextBox").value.trim();
+    let phone = document.querySelector("#userPhoneTextBox").value.trim();
+    let email = document.querySelector("#userEmailTextBox").value.trim();
+    let address = document.querySelector("#userAddressTextBox").value.trim();
+    let error = "";
+
+    let phoneRegex = /^\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    let emailRegex = /^\w+\@\w+(\.\w{2,})+$/;
+    let addressRegex = /^\d+(\-?[\d\w]+)*[\,\s]\s?\w+$/; // Accepts a: "#(-#*/X*), Street Name" Format, (i.e. 123-A, Main Street)
+
+    // Validate Name
+    if (name === "") {
+      error += "Enter Your Name<br />";
+    }
+
+    // Validate Phone Number
+    if (phone === "") {
+      error += "Enter Your Phone Number<br />";
+    } else if (!phoneRegex.test(phone)) {
+      error += "Invalid Phone Number<br />";
+    }
+
+    // Validate Email
+    if (email === "") {
+      error += "Enter Your Email<br />";
+    } else if (!emailRegex.test(email)) {
+      error += "Invalid Email<br />";
+    }
+
+    // Validate Address
+    if (address === "") {
+      error += "Enter Your Address<br />";
+    } else if (!addressRegex.test(address)) {
+      error += "Invalid Address<br />";
+    }
+
+    // Validate Order Type
+    if (
+      !document.querySelector("#pickUpRdio").checked &&
+      !document.querySelector("#deliveryRdio").checked
+    ) {
+      error += "Select an Order Type (Pickup or Delivery)<br />";
+    }
+
+    if (error !== "") {
+      throw new Error(error);
+    }
+  }
+
+  // Displays a message inside statusText, with a class name. Clears after a few seconds.
+  async function displayStatus(message, className) {
+    let statusText = document.querySelector("#statusText");
+    statusText.innerHTML = message;
+    statusText.className = className;
+
+    setTimeout(() => {
+      statusText.innerHTML = "";
+    }, 5000);
+  }
+});
