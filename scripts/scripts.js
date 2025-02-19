1b62256424bf7abe3ec0bbc43bce67a113fdb833
TheLostMrJ <greenslade_12@hotmail.com>
2025-02-19 13:38:37 -0330

    js start for reviews
    review box design and js before adding carasol

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,4 +6,44 @@
   Dates:  February 17, 2025 - 
 */
 
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  // Read reviewfrom JSON file
+  function generateReviews() {
+    let reviewPromise = fetch("../data/reviews.json");
+
+    reviewPromise
+      .then((response) => {
+        if (response.status === 200) {
+          response.json().then((data) => {
+            createReviewElement(data);
+          });
+        }
+      })
+      .catch((reject) => alert(reject));
+  }
+
+  function createReviewElement(data) {
+    data.forEach((element) => {
+      let reviewElement = document.createElement("div");
+      reviewElement.className = "review-card";
+      let raiting = element.rating;
+      let stars = "";
+      for (let i = 0; i < 5; i++) {
+        if (i < raiting) {
+          stars += "★";
+        } else stars += "☆";
+      }
+
+      let reviewElementHTML = `<h3>${element.firstName} ${element.lastName}</h3>
+                             <p>${element.date}</p>
+                             <p class = "star">${stars}</p>
+                             <p>${element.review}</p>`;
+
+      reviewElement.innerHTML += reviewElementHTML;
+
+      document.querySelector("#review-section").appendChild(reviewElement);
+    });
+  }
+
+  generateReviews();
+});
