65f98026b5ff533db0c141b5d7cc0a93c17ceeec
TheLostMrJ <greenslade_12@hotmail.com>
2025-02-20 15:02:53 -0330

    review update and some general css updates
    review update and some general css updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,43 +6,70 @@
   Dates:  February 17, 2025 - 
 */
 
-window.addEventListener("DOMContentLoaded", () => {
-  // Read reviewfrom JSON file
+window.addEventListener("DOMContentLoaded", function () {
+  // Read review from JSON file
   function generateReviews() {
     let reviewPromise = fetch("../data/reviews.json");
 
     reviewPromise
-      .then((response) => {
+      .then(function (response) {
         if (response.status === 200) {
-          response.json().then((data) => {
+          response.json().then(function (data) {
             createReviewElement(data);
           });
         }
       })
-      .catch((reject) => alert(reject));
+      .catch(function (reject) {
+        alert(reject);
+      });
   }
 
   function createReviewElement(data) {
-    data.forEach((element) => {
+    let reviewContainer = document.querySelector("#review-section");
+    let currentIndex = 0;
+
+    function displayReview() {
       let reviewElement = document.createElement("div");
-      reviewElement.className = "review-card";
-      let raiting = element.rating;
+      reviewElement.className = "review-card fade";
+
+      let rating = data[currentIndex].rating;
       let stars = "";
+
       for (let i = 0; i < 5; i++) {
-        if (i < raiting) {
+        if (i < rating) {
           stars += "★";
-        } else stars += "☆";
+        } else {
+          stars += "☆";
+        }
       }
 
-      let reviewElementHTML = `<h3>${element.firstName} ${element.lastName}</h3>
-                             <p>${element.date}</p>
-                             <p class = "star">${stars}</p>
-                             <p>${element.review}</p>`;
+      let reviewElementHTML = `<h3>${data[currentIndex].firstName} ${data[currentIndex].lastName}</h3>
+                               <p>${data[currentIndex].date}</p>
+                               <p class="star">${stars}</p>
+                               <p>${data[currentIndex].review}</p>`;
 
-      reviewElement.innerHTML += reviewElementHTML;
+      reviewElement.innerHTML = reviewElementHTML;
 
-      document.querySelector("#review-section").appendChild(reviewElement);
-    });
+      // Add review to container
+      reviewContainer.innerHTML = "";
+      reviewContainer.appendChild(reviewElement);
+
+      // Fade in effect
+      setTimeout(function () {
+        reviewElement.style.opacity = 1;
+      }, 50);
+
+      // Fade out effect after (after 5 seconds)
+      setTimeout(function () {
+        reviewElement.style.opacity = 0;
+      }, 4000);
+
+      // Move to the next review after 5 seconds
+      currentIndex = (currentIndex + 1) % data.length;
+      setTimeout(displayReview, 5000);
+    }
+
+    displayReview();
   }
 
   generateReviews();
